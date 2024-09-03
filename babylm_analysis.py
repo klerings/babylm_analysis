377ed6005946d6f167443d710c2db76fbc306726
aklerings <alina.klerings@uni-mannheim.de>
2024-09-03 10:36:28 +0200

    ready for top neurons

--- babylm_analysis.py
+++ babylm_analysis.py
@@ -18,6 +18,7 @@ from nnsight import NNsight
 import importlib.util
 import pickle
 import sys
+import json
 
 from memory_profiler import profile
 
@@ -142,6 +143,7 @@ def _pe_ig(
         submodules,
         mean_act_files,
         metric_fn,
+        pad_len,
         steps=10,
         metric_kwargs=dict()):
     tracer_kwargs = {'validate' : False, 'scan' : False}
@@ -191,7 +193,7 @@ def _pe_ig(
                         metrics.append(metric_fn(model, **metric_kwargs))
                 else:
                     with tracer.invoke(clean, pixel_values=img_inputs, scan=tracer_kwargs['scan']):
-                        submodule.output = f
+                        submodule.output = f[:,-pad_len:,:]   # we are only interested in the text tokens which are appended to img tokens
                         metrics.append(metric_fn(model, **metric_kwargs))
             metric = sum([m for m in metrics])
             metric.sum().backward(retain_graph=True) # TODO : why is this necessary? Probably shouldn't be, contact jaden
@@ -220,6 +222,7 @@ def _pe_ig_patch(
         model,
         submodules,
         metric_fn,
+        pad_len,
         steps=10,
         metric_kwargs=dict()):
     tracer_kwargs = {'validate' : False, 'scan' : False}
@@ -269,7 +272,7 @@ def _pe_ig_patch(
                 f.retain_grad()
                 fs.append(f)
                 with tracer.invoke(clean, scan=tracer_kwargs['scan']):
-                    submodule.output = f
+                    submodule.output = f[:,-pad_len:,:]
                     metrics.append(metric_fn(model, **metric_kwargs))
                 
             metric = sum([m for m in metrics])
@@ -293,7 +296,7 @@ def _pe_ig_patch(
     
     return (effects, deltas, grads)
 
-def get_important_neurons(examples, batch_size, mlps, mean_act_files, task):
+def get_important_neurons(examples, batch_size, mlps, pad_len, mean_act_files, task):
     # uses attribution patching to identify most important neurons for subtask
     num_examples = len(examples)
     batches = [examples[i:min(i + batch_size, num_examples)] for i in range(0, num_examples, batch_size)]
@@ -327,6 +330,7 @@ def get_important_neurons(examples, batch_size, mlps, mean_act_files, task):
                 mlps,
                 mean_act_files,
                 metric,
+                pad_len,
                 steps=10,
                 metric_kwargs=dict())
         
@@ -350,6 +354,7 @@ def get_important_neurons(examples, batch_size, mlps, mean_act_files, task):
                 model,
                 mlps,
                 metric,
+                pad_len,
                 steps=10,
                 metric_kwargs=dict())
         
@@ -373,29 +378,38 @@ def get_important_neurons(examples, batch_size, mlps, mean_act_files, task):
 
 
 if __name__ == "__main__":
-    task = sys.argv[1]
-    num_examples = int(sys.argv[2])
-    local = True if sys.argv[3] == "local" else False
-    print(f"task: {task} num_examples: {num_examples} ({type(num_examples)}) local: {local} ({type(local)})")
 
-    model_path = "git_1vd125_s1"
-    epoch = 23
+    ############ Parse Config ##############
+    with open(f"configs/{sys.argv[1]}", "r") as f:
+        config = json.load(f)
+    task = config["task"]
+    num_examples = config["num_examples"]
+    local = config["local"]
+    print(f"task: {task} num_examples: {num_examples},  local: {local}")
+
+    model_path = config["model_path"]
+    epoch = config["epoch"]
+
+    batch_size = config["batch_size"]
+    pad_len = config["pad_len"]
+
+    noimg = config["noimg"]
+    threshold_subtask = config["threshold_subtask"]
+
+
     # load and prepare model
     model, tokenizer, img_processor = load_model(model_path, epoch, own_model=True, local=local)
     submodules = extract_submodules(model)
     mlps = [submodules[submodule] for submodule in submodules if submodule.startswith("mlp")]
     print("loaded model and submodules")
 
-    batch_size = 4 #2 works
-    noimg = False
 
     # load and prepare data
     if task == "vqa":
-        examples = load_vqa_examples(tokenizer, img_processor, pad_to_length=32, n_samples=num_examples, local=local)
+        examples = load_vqa_examples(tokenizer, img_processor, pad_to_length=pad_len, n_samples=num_examples, local=local)
         subtask_key = "question_type"
     elif task == "blimp":
-        noimg = True
-        examples = load_blimp_examples(tokenizer, pad_to_length=32, n_samples=num_examples)
+        examples = load_blimp_examples(tokenizer, pad_to_length=pad_len, n_samples=num_examples)
         subtask_key = "linguistics_term"
         mean_act_files=None
     else:
@@ -404,39 +418,44 @@ if __name__ == "__main__":
 
     # precompute mean activations on task or retrieve precomputed activation files
     if task != "blimp":
-        prefix = f"{task}_{model_path}"
+        prefix = f"{task}_{model_path}_e{epoch}_n{num_examples if num_examples != -1 else 'all'}{'_noimg' if noimg else ''}"
         mean_act_files = []
         for file in os.listdir("mean_activations/"):
-            if file.startswith(prefix):
+            if file.startswith(prefix+"_mean_acts"):
                 mean_act_files.append(f"mean_activations/{file}")
         if len(mean_act_files) == 0:
             mean_act_files = compute_mean_activations(examples, model, mlps, batch_size=128, noimg=noimg, file_prefix=prefix)
-
-        print(f"computed mean activations")
+            print(f"computed mean activations")
+        else:
+            print("retrieved precomputed mean activations")
+    else:
+        print("using counterfactuals instead of mean activations")
 
     # identify subtasks
     subtasks = {}
     for e in examples:
         subtask = e[subtask_key]
-        if subtask in subtasks:
-            subtasks[subtask].append(e)
+        substask_nw = subtask.replace(" ", "")
+        if substask_nw in subtasks:
+            subtasks[substask_nw].append(e)
         else:
-            subtasks[subtask] = [e]
+            subtasks[substask_nw] = [e]
 
     print("extracted subtasks")
     final_subtasks = {}
     for s, e in subtasks.items():
         num_e = len(e)
-        if num_e >= 10:
+        if num_e >= threshold_subtask:
             print(f"{s}: {len(e)}")
             final_subtasks[s] = e
 
-    exit()
-
     # for each subtask, compute top neurons and save
     for subtask, examples in final_subtasks.items():
-        top_neurons = get_important_neurons(examples, batch_size, mlps, mean_act_files, task=task)
+        top_neurons = get_important_neurons(examples, batch_size, mlps, pad_len, mean_act_files, task=task)
         print(f"finished subtask: {subtask}")
 
-        with open(f"data/top_neurons/{task}/{model_path}_e{epoch}_{subtask}_top_neurons_per_subtask.pkl", "wb") as f:
+        out_dir = f"data/top_neurons/{task}/{prefix}/"
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        with open(f"data/top_neurons/{task}/{prefix}/{subtask}_top_neurons.pkl", "wb") as f:
             pickle.dump(top_neurons, f)
